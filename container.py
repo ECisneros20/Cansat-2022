285d2d174582764ecf8271533ccdf1893193ef92
ECisneros20 <enzo.cisneros@pucp.edu.pe>
2022-07-16 17:53:19 -0500

    0.0.2

--- container.py
+++ container.py
@@ -1,7 +1,5 @@
 import time
-from time import sleep
-import math
-import numpy
+import datetime as dt
 import csv
 
 # BMP280
@@ -15,12 +13,6 @@ from ina219 import DeviceRangeError
 # NEOM8N
 import pigpio
 import os
-
-# DS3231
-import SDL_DS3231
-
-# MG90S / 2N7000 / LED / Buzzer / motor / IRF530
-import RPi.GPIO as GPIO
 
 # Basic Inicialization
 TEAM_ID = '1085'
@@ -44,29 +36,15 @@ GPS_SATS = 0 # Number of GPS satellites being tracked by the GPS receiver
 SOFTWARE_STATE = 'LAUNCH_WAIT' # LAUNCH_WAIT / ASCENT / ROCKET_SEPARATION / DESCENT / TP_RELEASE / LANDED / SIMULATION
 CMD_ECHO = '' # Command like CXON / ST101325
 # TEAM_ID + MISSION_TIME + PACKET_COUNT + PACKET_TYPE + MODE + TP_RELEASED + ALTITUDE + TEMP + VOLTAGE + GPS_TIME + GPS_LATITUDE + GPS_LONGITUDE + GPS_ALTITUDE + GPS_SATS + SOFTWARE_STATE + CMD_ECHO
-MESSAGE = ''
-MESSAGE_TP = ''
 
 States = ['LAUNCH_WAIT', 'ASCENT', 'ROCKET_SEPARATION', 'DESCENT', 'TP_RELEASE', 'LANDED', 'SIMULATION']
 st = 0
-telemetry = "OFF"
-telemetry_TP = 'OFF'
-
-# Initialize XBEE S3B
-PORT = "/dev/ttyAMA0" # CONTAINER
-BAUD_RATE = 1200
-ADDR_GS = "0013A20041983FCF" # GROUND STATION
-ADDR_PAYLOAD = "0013A200419842C2" # PAYLOAD
-SIM_ENABLE = 0
-SIM_ACTIVATE = 0
-SIM_DISABLE = 0
 
 # Initialize BMP280
 bus = SMBus(1)
 bmp280 = BMP280(i2c_dev=bus)
 pressure_ground_level = 1013.25 # hPa ACTUALIZAR
-altitude_zero = 635 # m
-pressure_simulation = 0
+altitude_zero = 100 # m
 
 # Initialize INA219
 SHUNT_OHMS = 0.1
@@ -76,46 +54,10 @@ ina.configure()
 # Initialize NEOM8N
 RX = 12
 
-# Initialize DS3231
-ds3231 = SDL_DS3231.SDL_DS3231(1, 0x68)
-ds3231.write_now()
-
-# Initialize MG90S
-servoPIN = 18
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN,GPIO.OUT)
-p = GPIO.PWM(servoPIN, 50)
-p.start(6)
-
-# Initialize Buzzer
-buzzPIN = 26
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(buzzPIN,GPIO.OUT)
-GPIO.output(buzzPIN,GPIO.LOW)
-
-# Initialize motor
-motorPIN = 19
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(motorPIN,GPIO.OUT)
-MOTOR = 0
-fla = 1
-z = 0
-
 # # Initialize camera
 # camera = picamera.PiCamera()
 # camera.resolution = (640, 480)
 # camera.framerate = 30
-
-def XBEE():
-    global PORT, BAUD_RATE, device
-    device = XBeeDevice(PORT, BAUD_RATE)
-    device.open()
-
-def spin_servo_parachute():
-    global servoPIN, p
-    p.ChangeDutyCycle(3.5)
 
 def get_altitude_zero():
     global pressure_ground_level, altitude_zero
@@ -132,16 +74,6 @@ def BMP280():
     temperature = bmp280.get_temperature()
     
     alt = ( (pressure_ground_level/pressure) ** (1/5.257) - 1) * (temperature+273.15) / 0.0065 - altitude_zero
-    
-    TEMP = round(temperature,1)
-    ALTITUDE = round(alt,1)
-
-def BMP280_simulation():
-    global ALTITUDE, TEMP, pressure_ground_level, altitude_zero, pressure_simulation
-
-    temperature = bmp280.get_temperature()
-    
-    alt = ( (pressure_ground_level/pressure_simulation) ** (1/5.257) - 1) * (temperature+273.15) / 0.0065 - altitude_zero
     
     TEMP = round(temperature,1)
     ALTITUDE = round(alt,1)
@@ -198,116 +130,28 @@ def NEOM8N():
         pi.bb_serial_read_close(RX)
         pi.stop()
 
-def launch_wait():
-    print('Launch wait started!')
+def save_csv(data):
+    # Set csv_name corresponding to each module
+    value_chain = data.split(',')
+    if value_chain[3] == 'C':
+        csv_name = "Flight_1085_C"
+    elif value_chain[3] == 'T':
+        csv_name = "Flight_1085_T"
+    path_csv = "./" + csv_name + ".csv"
+    with open(path_csv, "a", newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(value_chain)
 
-    global st, ALTITUDE
+if __name__ == "__main__":
 
-    while ALTITUDE <= 10 and st == 0:
-        pass
-    st = 1
-
-    print('Launch wait completed!')
-
-def ascent():
-    print('Ascent started!')
-
-    global st, ALTITUDE
-
-    temporal = ALTITUDE
-
-    while ALTITUDE - temporal >= 0:
-        pass
-    st = 2
-
-    print('Ascent completed!')
-
-def rocket_separation():
-    print('Rocket separation started!')
-
-    global st
-
-    temporal = time.time_ns()
-
-    while time.time_ns() - temporal < 3000000000:
-        pass
-    st = 3
-
-    print('Rocket separation completed!')
-
-def descent():
-    print('Descent started!')
-
-    global st, ALTITUDE
-
-    while ALTITUDE > 400:
-        pass
-
-    spin_servo_parachute()
-
-    while ALTITUDE > 300:
-        pass
-
-    st = 4
-
-    print('Descent completed!')
-
-def tp_release():
-    print('TP release started!')
-
-    global st, motorPIN
-
-    temporal = time.time()
-
-    while time.time() - temporal < 5:
-        GPIO.output(motorPIN, GPIO.HIGH)
-    GPIO.output(motorPIN, GPIO.LOW)
-
-    while ALTITUDE > 1:
-        pass
-
-    st = 5
-
-    print('TP release completed!')
-
-def landed():
-    print('Process finished!')
-    GPIO.output(buzzPIN,GPIO.HIGH)
-    sleep(300)
-    GPIO.output(buzzPIN,GPIO.LOW)
-
-def flight_mode():
-    launch_wait()
-    ascent()
-    rocket_separation()
-    descent()
-    tp_release()
-    landed()
-
-def simulation_mode():
-    launch_wait()
-    ascent()
-    rocket_separation()
-    descent()
-    tp_release()
-    landed()
-
-if _name_ == "_main_":
-
-    XBEE()
-
-    _thread.start_new_thread(readGroundStation,())
-    _thread.start_new_thread(toGSfromC,())
-    _thread.start_new_thread(toGSfromTP,())
+    get_altitude_zero()
     
     while (1):
-        if SIM_DISABLE:
-            print('Flight mode!')
-            get_altitude_zero()
-            flight_mode()
-        elif SIM_ENABLE and SIM_ACTIVATE:
-            print('Simulation mode!')
-            get_altitude_zero()
-            simulation_mode()
-        else:
-            print('Select a mode!')
+        print('Flight mode!')
+        BMP280()
+        INA219()
+        NEOM8N()
+        MISSION_TIME = str(dt.datetime.utcnow().hour) + ':' + str(dt.datetime.utcnow().minute) + ':' + str(dt.datetime.utcnow().second) + '.' + str(dt.datetime.utcnow().microsecond)[0:2]
+        MESSAGE = str(TEAM_ID) + ',' + str(MISSION_TIME) + ',' + str(PACKET_COUNT) + ',' + str(PACKET_TYPE) + ',' + str(MODE) + ',' + str(TP_RELEASED) + ',' + str(ALTITUDE) + ',' + str(TEMP) + ',' + str(VOLTAGE) + ',' + str(GPS_TIME) + ',' + str(GPS_LATITUDE) + ',' + str(GPS_LONGITUDE) + ',' + str(GPS_ALTITUDE) + ',' + str(GPS_SATS) + ',' + str(SOFTWARE_STATE) + ',' + str(CMD_ECHO)
+        save_csv(MESSAGE)
+        PACKET_COUNT += 1
